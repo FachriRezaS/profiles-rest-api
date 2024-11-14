9e00e8ea5c42195754c12f1c77671cf85e3eba8d
Fachri Reza <fachri.reza@sirclo.com>
2024-11-14 12:46:03 +0700

    add post, put, patch, delete method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,12 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
 
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         """Returns a list of APIView features"""
-
         an_apiview = [
             'Uses HTTP methods as functions (get, post, patch, put, delete)',
             'Is similar to a traditional Django View',
@@ -15,3 +19,26 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+    
+    def post(self, request):
+        """create post request"""
+        serializers = self.serializer_class(data=request.data)
+
+        if serializers.is_valid():
+            name = serializers.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """update post request"""
+        return Response({'method': 'PUT'})
+    
+    def patch(self, request, pk=None):
+        """partial update post request"""
+        return Response({'method': 'PATCH'})
+    
+    def delete(self, request, pk=None):
+        """delete post request"""
+        return Response({'method': 'DELETE'})
